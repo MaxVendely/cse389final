01a785298ad726f8197840d79f068b763a28c143
MaxVendely <maxvendely@gmail.com>
2025-05-09 09:51:34 -0400

    Imported Basic AI Code

---
+++ Source/cse389final/ChasingFlag_AIEnemy.cpp
@@ -0,0 +1,130 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ChasingFlag_AIEnemy.h"
+#include "PatrollingAIController.h"
+#include "Navigation/PathFollowingComponent.h"
+#include "AITypes.h"
+#include "ThePlayer.h"
+
+// Sets default values
+AChasingFlag_AIEnemy::AChasingFlag_AIEnemy()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	PlayerCollisionDetection = CreateDefaultSubobject<USphereComponent>(TEXT("Player Collision Detection"));
+	PlayerCollisionDetection->SetupAttachment(RootComponent);
+
+	PlayerAttackCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("Player Attack Collision"));
+	PlayerAttackCollision->SetupAttachment(GetMesh(), TEXT("Wolf_-Head"));
+
+	PlayerDetected = false;
+	CanAttackPlayer = false;
+}
+
+// Called when the game starts or when spawned
+void AChasingFlag_AIEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	AIController = Cast<APatrollingAIController>(GetController());
+	AIController->GetPathFollowingComponent()->OnRequestFinished.AddUObject(this, &AChasingFlag_AIEnemy::OnAIMoveCompleted);
+
+	PlayerCollisionDetection->OnComponentBeginOverlap.AddDynamic(this,
+		&AChasingFlag_AIEnemy::OnPlayerDetectedOverlapBegin);
+
+	PlayerCollisionDetection->OnComponentEndOverlap.AddDynamic(this,
+		&AChasingFlag_AIEnemy::OnPlayerDetectedOverlapEnd);
+
+	PlayerAttackCollision->OnComponentBeginOverlap.AddDynamic(this,
+		&AChasingFlag_AIEnemy::OnPlayerAttackOverlapBegin);
+
+	PlayerAttackCollision->OnComponentEndOverlap.AddDynamic(this,
+		&AChasingFlag_AIEnemy::OnPlayerAttackOverlapEnd);
+}
+
+// Called every frame
+void AChasingFlag_AIEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AChasingFlag_AIEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AChasingFlag_AIEnemy::OnAIMoveCompleted(FAIRequestID RequestID, const FPathFollowingResult& Result)
+{
+	if (!PlayerDetected) {
+		AIController->RandomPatrol();
+	}
+}
+
+void AChasingFlag_AIEnemy::MoveToPlayer()
+{
+	if (Player) {
+		AIController->MoveToLocation(Player->GetActorLocation(), StoppingDistance, true);
+	}
+}
+
+void AChasingFlag_AIEnemy::SeekPlayer()
+{
+	MoveToPlayer();
+	GetWorld()->GetTimerManager().SetTimer(SeekPlayerTimerHandle, this,
+		&AChasingFlag_AIEnemy::SeekPlayer, 0.25f, true);
+}
+
+void AChasingFlag_AIEnemy::StopSeekingPlayer()
+{
+	GetWorld()->GetTimerManager().ClearTimer(SeekPlayerTimerHandle);
+}
+
+void AChasingFlag_AIEnemy::OnPlayerDetectedOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Player = Cast<AThePlayer>(OtherActor);
+	if (Player) {
+		PlayerDetected = true;
+		SeekPlayer();
+	}
+}
+
+void AChasingFlag_AIEnemy::OnPlayerDetectedOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	Player = Cast<AThePlayer>(OtherActor);
+	if (Player) {
+		PlayerDetected = false;
+		StopSeekingPlayer();
+		AIController->RandomPatrol();
+	}
+}
+
+void AChasingFlag_AIEnemy::OnPlayerAttackOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Player = Cast<AThePlayer>(OtherActor);
+	if (Player) {
+		PlayerDetected = true;
+		CanAttackPlayer = true;
+		UE_LOG(LogTemp, Warning, TEXT("Player Damaged"));
+	}
+}
+
+void AChasingFlag_AIEnemy::OnPlayerAttackOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	Player = Cast<AThePlayer>(OtherActor);
+	if (Player) {
+		CanAttackPlayer = false;
+
+		SeekPlayer();
+	}
+}
+
+bool AChasingFlag_AIEnemy::GetCanAttackPlayer()
+{
+	return CanAttackPlayer;
+}
+
---
+++ Source/cse389final/ChasingFlag_AIEnemy.h
@@ -0,0 +1,78 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Components/SphereComponent.h"
+#include "Components/BoxComponent.h"
+#include "ChasingFlag_AIEnemy.generated.h"
+
+UCLASS()
+class CSE389FINAL_API AChasingFlag_AIEnemy : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AChasingFlag_AIEnemy();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	bool PlayerDetected;
+	bool CanAttackPlayer;
+
+	class AThePlayer* Player;
+
+	UPROPERTY(EditAnywhere)
+		class USphereComponent* PlayerCollisionDetection;
+
+	UPROPERTY(EditAnywhere)
+		class UBoxComponent* PlayerAttackCollision;
+
+	class APatrollingAIController* AIController;
+
+	void OnAIMoveCompleted(struct FAIRequestID RequestID, const struct FPathFollowingResult& Result);
+
+	UPROPERTY(EditAnywhere)
+		float StoppingDistance = 200.f;
+
+	FTimerHandle SeekPlayerTimerHandle;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION()
+		void MoveToPlayer();
+
+	UFUNCTION()
+		void SeekPlayer();
+
+	UFUNCTION()
+		void StopSeekingPlayer();
+
+	UFUNCTION()
+		void OnPlayerDetectedOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+			class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+		void OnPlayerDetectedOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+			class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UFUNCTION()
+		void OnPlayerAttackOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+			class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+		void OnPlayerAttackOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+			class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UFUNCTION(BlueprintCallable)
+		bool GetCanAttackPlayer();
+
+};
